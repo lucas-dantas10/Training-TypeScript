486f1fb9442af0daa226bf55d18e36c3f91afbbe
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-04 12:25:59 -0300

    criando um métoda cria negociacao

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,11 +12,15 @@ export class NegociacaoController {
     }
 
     adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+
+    criaNegociacao() {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValue.value);
-        const negociacao = new Negociacao(date, quantidade, valor);
-        console.log(negociacao);
+        return new Negociacao(date, quantidade, valor);
     }
 }
