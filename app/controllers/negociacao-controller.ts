9bc31127c66cc869fc25171b04937a5744455efd
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-04 12:37:15 -0300

    criando o método limparFormulario

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -11,16 +11,24 @@ export class NegociacaoController {
         this.inputValue = document.querySelector("#valor");
     }
 
-    adiciona() {
+    adiciona(): void {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        this.limparFormulario();
     }
 
-    criaNegociacao() {
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValue.value);
         return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputValue.value = '';
+        this.inputQuantidade.value = '';
+        this.inputData.focus();
     }
 }
