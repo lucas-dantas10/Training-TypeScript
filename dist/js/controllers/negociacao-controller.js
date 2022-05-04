c7e9344a550689df5e9fad4fb398b3bb32fb6814
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-04 11:46:23 -0300

    criando a negociação controller

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,11 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValue = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = new Negociacao(this.inputData.value, this.inputQuantidade.value, this.inputValue.value);
+    }
+}
