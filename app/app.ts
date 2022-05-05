98c376a8fcdb9a6261cbedb162b665565b3a5463
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-05 14:06:43 -0300

    testando nova classe

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { Negociacoes } from "./models/negociacoes.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector(".form");
