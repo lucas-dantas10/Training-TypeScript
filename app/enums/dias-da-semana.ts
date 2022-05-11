68b809e78b2d725e4dce9dcf8e112ef194bd37e0
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-11 15:43:28 -0300

    criando uma Enumeration com os dias da semana

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,9 @@
+export enum DiasDaSemana { //ENUM = somente leitura(readonly) por padrão
+    DOMINGO = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 4,
+    SEXTA = 5,
+    SABADO = 6
+}
